5b8d027d57605f7c271ce6ff42a7239496dad688
xuan10116 <83226890+xuan10116@users.noreply.github.com>
2021-04-27 16:36:07 +0800

    add test pull
    add test pull

--- src/main/java/dao/userdao.java
+++ src/main/java/dao/userdao.java
@@ -2,6 +2,6 @@ package dao;
 
 public class userdao {
     public String getName(){
-        return "hot-fix"+"newnewname"+"master update"+"newnewname"+"commit2"+"push test twice";
+        return "hot-fix"+"newnewname"+"master update"+"newnewname"+"commit2"+"push test twice"+"test pull";
     }
 }
