c8d051c08ea2d4b8ebe086354e7a28df5365976a
xuan <lplfcm@outlook.com>
2021-04-27 16:20:48 +0800

    master push test

--- src/main/java/dao/userdao.java
+++ src/main/java/dao/userdao.java
@@ -2,6 +2,6 @@ package dao;
 
 public class userdao {
     public String getName(){
-        return "hot-fix"+"newnewname"+"master update"+"newnewname"+"commit2";
+        return "hot-fix"+"newnewname"+"master update"+"newnewname"+"commit2"+"push test";
     }
 }
